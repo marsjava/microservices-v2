61e61749863993c11956241fc18fd21d2d594209
marsjava <marikannan.cvp@gmail.com>
2021-01-26 12:12:16 +0530

    Step 13 Configure JPA and initialized Data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(1001, 'USD','INR',73.04,'')
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(1002, 'EUR','INR',88.59,'')
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(1003, 'AUD','INR',56.13,'')
